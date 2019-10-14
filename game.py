d8b93b8e6bbff94d8011c2ab7fa89334b2e2c5c6
Chesare95 <Chesare95@yandex.ru>
2019-10-14 23:24:09 +0300

    Переписал структуру. Теперь число - это объект класса, который может себя изменять. В файле game.py так же осталась игра. В модуле game_analys.py собрана основная логика и класс реализации (Я в термины хз). Модуль game_cycle.py не доделан, но предназначен для прогона алгоритма решения на рандомных числах с записью результатов в файл.

--- game.py
+++ game.py
@@ -1,173 +1,53 @@
-import random
-import time
+from game_analys import *
 
-def _list(x):
-	res = []
-	while x > 0:
-		res.append(x % 10)
-		x //= 10
-	res.reverse()
-	return res
-
-def _int(L):
-	num = 0
-	for i, v in enumerate(reversed(L)):
-		num += v * 10 ** i
-	return num
-	
-def outnum(L):
-	res=""
-	for i in L:
-		res += str(i)
-	return res
-
-def make_move(pos, sign):
-	global gameList, step
-	pos = int(pos)
-	# step_move = 0
-	# pos1, sign1 = pos, sign
-	if sign == "+":
-		if gameList[pos-1]-1==-1:
-			gameList[pos-1] = gameList[pos-1] + 1 if gameList[pos-1]!=9 else 0
-		else:
-			gameList[gameList[pos-1]-1] = gameList[gameList[pos-1]-1]-1 if gameList[gameList[pos-1]-1]!=0 else 9
-			gameList[pos-1] = gameList[pos-1] + 1 if gameList[pos-1]!=9 else 0
-			
-	elif sign == "-":
-		if gameList[pos-1]-1==-1:
-			gameList[pos-1] = gameList[pos-1] - 1 if gameList[pos-1]!=0 else 9
-		else:
-			gameList[gameList[pos-1]-1] =gameList[gameList[pos-1]-1] + 1 if gameList[gameList[pos-1]-1]!=9 else 0
-			gameList[pos-1] = gameList[pos-1] - 1 if gameList[pos-1]!=0 else 9
-	step +=1
-
-	#if step==40:		print("+++")
-	print("\nТвое число: " + outnum(gameList) + " " + str(pos), sign, str(step) + "\nПозиции:    123456789")		
-
-def zeroing(pos, q=0):
-	flag = gameList[pos-1]
-	while gameList[pos-1] !=0 or gameList[pos-1]==pos:
-		if gameList[pos-1] > pos: make_move(pos,"+")
-		elif gameList[pos-1] < pos: make_move(pos,"-")
-		else: break
-	if flag!=1 and q!=0:
-		rep = pos if gameList[pos-2]==0 else pos+1
-		for j in range(2, rep):
-			zeroing(j)
-def not_problem():
-	global gameList
-	
-	for i in range(2,10):
-		zeroing(i); 
-		sw = i if gameList[i-2]==0 else i+1
-		for j in range(2, sw):
-			zeroing(j, 1)
-
-def reshator():
-	global gameList
-	
-	while _int(gameList[1:])!=0:
-		not_problem()
-		
-		problem=[]
-		problem = problemator()
-		
-		if problem:
-			for i in range(9):
-				if gameList[i] == 0 and i+1>problem[0]: 
-					for j in range(problem[0]): make_move(i+1,"+"); 
-					make_move(i+1,"-")
-					zeroing(problem[0])
-				elif gameList[i] == 0 and i+1<problem[0]: 
-					for j in range(problem[0]): make_move(i+1,"-"); 
-					make_move(i+1,"+")
-					zeroing(problem[0])
-				else: continue
-		not_problem()		
-				
-		
-		
-		if _int(gameList[1:])!=0 and _int(gameList[1:])>10:		
-			for i in gameList[1:]:
-				if i==0: continue
-				else:
-					for j in range(1,i+1):
-						make_move(9,"+"); 
-			make_move(9,"-")
-		if _int(gameList[1:-1])==0 and gameList[-1]==9: make_move(2,"-")
-
-def problemator():
-	global gameList, step
-	problempos = []
-	for i in range(len(gameList)):
-		if i+1==gameList[i]: problempos.append(i+1)
-	return problempos
-
-
-def new_game(numb=0):
-	new_numb = numb if numb!=0 else random.randint(100000000, 999999999)
-	#new_numb = 863259971 #104
-	print("\nТвое число: " + str(new_numb) + "\nПозиции:    123456789")
-	return _list(new_numb)
-	
 game = True
-while game:	
-#start_time = time.time()	
-	hi = input("Сыграем? y/n  ")
+while game:  # 925607913  84 / 104
+    hi = input("Сыграем? y/n  ")
 
-	if hi=="y":
+    if hi == "y":
+        new_numb = NewGame(925607913)
+        gameList = new_numb.gameList
 
-		gameList = new_game()
-		step=0
+        flag = True if list_int(gameList) != 123456789 else False
+        while flag:
+            vvod = input("Делай ход: ")
+            move = tuple(vvod)
 
-		flag = True if _int(gameList) != 123456789 else False
-		while flag:
-			#vvod = "lol"
-			vvod = input("Делай ход: ")
-			move = tuple(vvod)
-			
-			if vvod == "lan": 
-				print("Сдался? Слабак!")
-				break
-			elif vvod == "pr": print(problemator())		
-			elif vvod == "lol":
-				reshator()				
-				if gameList[0]==0:
-					print("Поздравляю, ты выйграл! Шагов %s" %step)
-					flag=False
-					continue
-				elif gameList[0] != 1:
-					for i in range(gameList[0],10): # когда не 0 только первый
-						make_move(i,"+")
-						make_move(i,"-")
-				else:	
-					for i in range(9): # когда первый 1
-						make_move(2,"+")
-						make_move(2,"-")
-					
-			elif len(move)!=2 or not move[0].isdigit():
-				print("Неправильно ты ходишь: вводи n+ или n-, n=1..9")
-				continue	
-			
-			else: make_move(*move); step +=1
-					
-			
-			if _int(gameList)==0:
-				print("Поздравляю, ты выйграл! Шагов %s" %step); flag=False
-			elif _int(gameList)==123456789:
-				print("Поздравляю, ты проиграл! Шагов %s" %step); flag=False
-		#else: print("Пока")
-	elif hi=="n": 
-		print("Пока"); game = False
-	else: print("Не понял ввод, повтори")
-		
+            if vvod == "lan":
+                print("Сдался? Слабак!")
+                break
+            elif vvod == "lol":
+                reshator(new_numb)
+                if gameList[0] == 0:
+                    print("Поздравляю, ты выйграл! Шагов %s" % new_numb.step)
+                    flag = False
+                    continue
+                elif gameList[0] != 1:
+                    for i in range(gameList[0], 10):  # когда не 0 только первый
+                        new_numb.make_move(i, "+")
+                        new_numb.make_move(i, "-")
+                else:
+                    for i in range(9):  # когда первый 1
+                        new_numb.make_move(2, "+")
+                        new_numb.make_move(2, "-")
 
+            elif len(move) != 2 or not move[0].isdigit():
+                print("Неправильно ты ходишь: вводи n+ или n-, n=1..9")
+                continue
 
+            else:
+                new_numb.make_move(*move)
 
-
-
-
-
-
-
-
+            if list_int(gameList) == 0:
+                print("Поздравляю, ты выйграл! Шагов %s" % new_numb.step)
+                #print(new_numb.gameList)
+                flag = False
+            elif list_int(gameList) == 123456789:
+                print("Поздравляю, ты проиграл! Шагов %s" % new_numb.step)
+                flag = False
+    # else: print("Пока")
+    elif hi == "n":
+        print("Пока")
+        game = False
+    else:
+        print("Не понял ввод, повтори")
--- game_analys.py
+++ game_analys.py
@@ -1,146 +1,125 @@
 import random
-import time
 import os
 import sys
 
-def _int(L):
-	num = 0
-	for i, v in enumerate(reversed(L)):
-		num += v * 10 ** i
-	return num
+
+def list_int(L):
+    num = 0
+    for i, v in enumerate(reversed(L)):
+        num += v * 10 ** i
+    return num
+
+
+def int_list(x):
+    res = []
+    while x > 0:
+        res.append(x % 10)
+        x //= 10
+    while len(res) < 9:
+        res.append(0)
+    res.reverse()
+    return res
+
+
+def outnum(L):
+    res = ""
+    for i in L:
+        res += str(i)
+    return res
+
 
 def reader_numb(path):
-	res = [(int(x[7:16]),int(x[24:-1])) for x in open(path).readlines()]
-	return res
-	
-def make_move(pos, sign):
-	global gameList, step
-	pos = int(pos)
-	# step_move = 0
-	# pos1, sign1 = pos, sign
-	if sign == "+":
-		if gameList[pos-1]-1==-1:
-			gameList[pos-1] = gameList[pos-1] + 1 if gameList[pos-1]!=9 else 0
-		else:
-			gameList[gameList[pos-1]-1] = gameList[gameList[pos-1]-1]-1 if gameList[gameList[pos-1]-1]!=0 else 9
-			gameList[pos-1] = gameList[pos-1] + 1 if gameList[pos-1]!=9 else 0
-			
-	elif sign == "-":
-		if gameList[pos-1]-1==-1:
-			gameList[pos-1] = gameList[pos-1] - 1 if gameList[pos-1]!=0 else 9
-		else:
-			gameList[gameList[pos-1]-1] =gameList[gameList[pos-1]-1] + 1 if gameList[gameList[pos-1]-1]!=9 else 0
-			gameList[pos-1] = gameList[pos-1] - 1 if gameList[pos-1]!=0 else 9
-	step +=1
-
-def zeroing(pos, q=0):
-	flag = gameList[pos-1]
-	while gameList[pos-1] !=0 or gameList[pos-1]==pos:
-		if gameList[pos-1] > pos: make_move(pos,"+")
-		elif gameList[pos-1] < pos: make_move(pos,"-")
-		else: break
-	if flag!=1 and q!=0:
-		rep = pos if gameList[pos-2]==0 else pos+1
-		for j in range(2, rep):
-			zeroing(j)
-			
-def not_problem():
-	global gameList
-	
-	for i in range(2,10):
-		zeroing(i); 
-		sw = i if gameList[i-2]==0 else i+1
-		for j in range(2, sw):
-			zeroing(j, 1)
-			
-def problemator():
-	global gameList, step
-	problempos = []
-	for i in range(len(gameList)):
-		if i+1==gameList[i]: problempos.append(i+1)
-	return problempos
-
-def reshator():
-	global gameList
-	
-	while _int(gameList[1:])!=0:
-		not_problem()
-		
-		problem=problemator()
-		#problem = 
-		if problem:
-			for i in range(9):
-				if gameList[i] == 0 and i+1>problem[0]: 
-					for j in range(problem[0]): make_move(i+1,"+"); #print("tut")
-					make_move(i+1,"-")
-					zeroing(problem[0])
-				elif gameList[i] == 0 and i+1<problem[0]: 
-					for j in range(problem[0]): make_move(i+1,"-"); #print("tut")
-					make_move(i+1,"+")
-					zeroing(problem[0])
-				else: continue
-		not_problem()		
-				
-		if _int(gameList[1:])!=0 and _int(gameList[1:])>10:		
-			for i in gameList[1:]:
-				if i==0: continue
-				else:
-					for j in range(1,i+1):
-						make_move(9,"+"); 
-			make_move(9,"-")
-		if _int(gameList[1:-1])==0 and gameList[-1]==9: make_move(2,"-")
+    res = [(int(x[7:16]), int(x[24:-1])) for x in open(path).readlines()]
+    return res
 
 
-def new_game(numb=0):
-	new_numb = numb if numb!=0 else [random.randint(0,9) for i in range(9)]
-	return new_numb
+def zeroing(number, pos, q=0):
+    flag = number.gameList[pos - 1]
+    while number.gameList[pos - 1] != 0 or number.gameList[pos - 1] == pos:
+        if number.gameList[pos - 1] > pos:
+            number.make_move(pos, "+")
+        elif number.gameList[pos - 1] < pos:
+            number.make_move(pos, "-")
+        else:
+            break
+    if flag != 1 and q != 0:
+        rep = pos if number.gameList[pos - 2] == 0 else pos + 1
+        for j in range(2, rep):
+            zeroing(number, j)
 
 
-path = os.path.dirname(os.path.abspath(sys.argv[0]))+"\mylisting.txt"
-f = open(path, 'w')
-f.write("{0:13}{1:10}{2:10}\n".format("Number","Step_old","Step_new"))
-#myList = reader_numb(r'C:\Users\Chesare\Desktop\Scripts Python\listing.txt')	
-
-start_time = time.time()	
-	
-for bla in range(100): 
-	gameList = new_game()
-	number = _int(gameList)
-	step=0
-	
-	flag = True if _int(gameList) != 123456789 else False
-	while flag:
-		
-		reshator()				
-
-		if gameList[0]==0:
-			f.write("{0:0>9}    {1:<10}\n".format(number,step))
-			flag=False
-			continue
-		elif gameList[0] != 1:
-			for i in range(gameList[0],10): # когда не 0 только первый
-				make_move(i,"+")
-				make_move(i,"-")
-		else:	
-			for i in range(9): # когда первый 1
-				make_move(2,"+")
-				make_move(2,"-")
-				
-		if _int(gameList)==0 and not flag:
-			f.write("{0:0>9}    {1:<10}\n".format(number,step))
-		elif _int(gameList)==123456789:
-			print("Поздравляю, ты проиграл! Шагов %s" %step); flag=False
-f.write("--- %s seconds ---" % (time.time() - start_time))
-f.close
-print("buy")
-		
+def not_problem(number):
+    for i in range(2, 10):
+        zeroing(number, i);
+        sw = i if number.gameList[i - 2] == 0 else i + 1
+        for j in range(2, sw):
+            zeroing(number, j, 1)
 
 
+def problemator(number):
+    problempos = []
+    for i in range(len(number.gameList)):
+        if i + 1 == number.gameList[i]: problempos.append(i + 1)
+    return problempos
 
 
+def reshator(number):
+    while list_int(number.gameList[1:]) != 0:
+        not_problem(number)
 
+        problem = problemator(number)
 
+        if problem:
+            for i in range(9):
+                if number.gameList[i] == 0 and i + 1 > problem[0]:
+                    for j in range(problem[0]): number.make_move(i + 1, "+")  # print("tut")
+                    number.make_move(i + 1, "-")
+                    zeroing(number, problem[0])
+                elif number.gameList[i] == 0 and i + 1 < problem[0]:
+                    for j in range(problem[0]): number.make_move(i + 1, "-")  # print("tut")
+                    number.make_move(i + 1, "+")
+                    zeroing(number, problem[0])
+                else:
+                    continue
+        not_problem(number)
 
+        if list_int(number.gameList[1:]) != 0 and list_int(number.gameList[1:]) > 10:
+            for i in number.gameList[1:]:
+                if i == 0:
+                    continue
+                else:
+                    for j in range(1, i + 1):
+                        number.make_move(9, "+")
+            number.make_move(9, "-")
+        if list_int(number.gameList[1:-1]) == 0 and number.gameList[-1] == 9: number.make_move(2, "-")
 
+class NewGame:
 
+    def __init__(self, numb, mode=0):
+        self.gameList = int_list(numb) if numb != 0 else [random.randint(0, 9) for i in range(9)]
+        self.step = 0
+        self.mode = mode
+        if not mode: print("\nТвое число: " + str(outnum(self.gameList)) + "\nПозиции:    123456789")
 
+    def make_move(self, pos, sign):
+        pos = int(pos)
+
+        if sign == "+":
+            if self.gameList[pos - 1] - 1 == -1:
+                self.gameList[pos - 1] = self.gameList[pos - 1] + 1 if self.gameList[pos - 1] != 9 else 0
+            else:
+                self.gameList[self.gameList[pos - 1] - 1] = self.gameList[self.gameList[pos - 1] - 1] - 1 if \
+                    self.gameList[self.gameList[pos - 1] - 1] != 0 else 9
+                self.gameList[pos - 1] = self.gameList[pos - 1] + 1 if self.gameList[pos - 1] != 9 else 0
+
+        elif sign == "-":
+            if self.gameList[pos - 1] - 1 == -1:
+                self.gameList[pos - 1] = self.gameList[pos - 1] - 1 if self.gameList[pos - 1] != 0 else 9
+            else:
+                self.gameList[self.gameList[pos - 1] - 1] = self.gameList[self.gameList[pos - 1] - 1] + 1 if \
+                    self.gameList[self.gameList[pos - 1] - 1] != 9 else 0
+                self.gameList[pos - 1] = self.gameList[pos - 1] - 1 if self.gameList[pos - 1] != 0 else 9
+        self.step += 1
+        if not self.mode:
+            print("\nТвое число: " + outnum(self.gameList) + " " + str(pos), sign,
+                  str(self.step) + "\nПозиции:    123456789")
