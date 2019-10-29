f7542f908eeef827e7da7ce153b74671b5d23606
Chesare95 <Chesare95@yandex.ru>
2019-10-29 22:11:05 +0300

    Засунул решатор в функции класса.

--- game_analys.py
+++ game_analys.py
@@ -1,11 +1,8 @@
 import random
-import os
-import sys
 
-
-def list_int(L):
+def list_int(l):
     num = 0
-    for i, v in enumerate(reversed(L)):
+    for i, v in enumerate(reversed(l)):
         num += v * 10 ** i
     return num
 
@@ -21,15 +18,17 @@ def int_list(x):
     return res
 
 
-def outnum(L):
+def outnum(l):
     res = ""
-    for i in L:
+    for i in l:
         res += str(i)
     return res
 
 
 def reader_numb(path):
-    res = [(int(x[7:16]), int(x[24:-1])) for x in open(path).readlines()]
+    with open(path, 'r') as f:
+        lines = f.readlines()[1:-1]
+    res = [(int(x[:9]), int(x[23:-1])) for x in lines]
     return res
 
 
@@ -50,7 +49,7 @@ def zeroing(number, pos, q=0):
 
 def not_problem(number):
     for i in range(2, 10):
-        zeroing(number, i);
+        zeroing(number, i)
         sw = i if number.gameList[i - 2] == 0 else i + 1
         for j in range(2, sw):
             zeroing(number, j, 1)
@@ -63,43 +62,17 @@ def problemator(number):
     return problempos
 
 
-def reshator(number):
-    while list_int(number.gameList[1:]) != 0:
-        not_problem(number)
 
-        problem = problemator(number)
-
-        if problem:
-            for i in range(9):
-                if number.gameList[i] == 0 and i + 1 > problem[0]:
-                    for j in range(problem[0]): number.make_move(i + 1, "+")  # print("tut")
-                    number.make_move(i + 1, "-")
-                    zeroing(number, problem[0])
-                elif number.gameList[i] == 0 and i + 1 < problem[0]:
-                    for j in range(problem[0]): number.make_move(i + 1, "-")  # print("tut")
-                    number.make_move(i + 1, "+")
-                    zeroing(number, problem[0])
-                else:
-                    continue
-        not_problem(number)
-
-        if list_int(number.gameList[1:]) != 0 and list_int(number.gameList[1:]) > 10:
-            for i in number.gameList[1:]:
-                if i == 0:
-                    continue
-                else:
-                    for j in range(1, i + 1):
-                        number.make_move(9, "+")
-            number.make_move(9, "-")
-        if list_int(number.gameList[1:-1]) == 0 and number.gameList[-1] == 9: number.make_move(2, "-")
 
 class NewGame:
 
-    def __init__(self, numb, mode=0):
+    def __init__(self, numb, mode=0, old_step=0):
         self.gameList = int_list(numb) if numb != 0 else [random.randint(0, 9) for i in range(9)]
         self.step = 0
+        self.old_step = old_step
         self.mode = mode
-        if not mode: print("\nТвое число: " + str(outnum(self.gameList)) + "\nПозиции:    123456789")
+        self.start = str(outnum(self.gameList))
+        if not mode: print("\nТвое число: " + self.start + "\nПозиции:    123456789")
 
     def make_move(self, pos, sign):
         pos = int(pos)
@@ -123,3 +96,33 @@ class NewGame:
         if not self.mode:
             print("\nТвое число: " + outnum(self.gameList) + " " + str(pos), sign,
                   str(self.step) + "\nПозиции:    123456789")
+
+    def reshator(self):
+        while list_int(self.gameList[1:]) != 0:
+            not_problem(self)
+
+            problem = problemator(self)
+
+            if problem:
+                for i in range(9):
+                    if self.gameList[i] == 0 and i + 1 > problem[0]:
+                        for j in range(problem[0]): self.make_move(i + 1, "+")  # print("tut")
+                        self.make_move(i + 1, "-")
+                        zeroing(self, problem[0])
+                    elif self.gameList[i] == 0 and i + 1 < problem[0]:
+                        for j in range(problem[0]): self.make_move(i + 1, "-")  # print("tut")
+                        self.make_move(i + 1, "+")
+                        zeroing(self, problem[0])
+                    else:
+                        continue
+            not_problem(self)
+
+            if list_int(self.gameList[1:]) != 0 and list_int(self.gameList[1:]) > 10:
+                for i in self.gameList[1:]:
+                    if i == 0:
+                        continue
+                    else:
+                        for j in range(1, i + 1):
+                            self.make_move(9, "+")
+                self.make_move(9, "-")
+            if list_int(self.gameList[1:-1]) == 0 and self.gameList[-1] == 9: self.make_move(2, "-")
