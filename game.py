8087c1bc414431b8497b0a52a1147fcba104dda9
Chesare95 <Chesare95@yandex.ru>
2019-10-29 22:16:20 +0300

    Поменял названия переменных, учел изменения в game_analys

--- game.py
+++ game.py
@@ -5,8 +5,8 @@ while game:  # 925607913  84 / 104
     hi = input("Сыграем? y/n  ")
 
     if hi == "y":
-        new_numb = NewGame(925607913)
-        gameList = new_numb.gameList
+        cur_numb = NewGame(925607913)
+        gameList = cur_numb.gameList
 
         flag = True if list_int(gameList) != 123456789 else False
         while flag:
@@ -17,33 +17,33 @@ while game:  # 925607913  84 / 104
                 print("Сдался? Слабак!")
                 break
             elif vvod == "lol":
-                reshator(new_numb)
+                cur_numb.reshator()
                 if gameList[0] == 0:
-                    print("Поздравляю, ты выйграл! Шагов %s" % new_numb.step)
+                    print("Поздравляю, ты выйграл! Шагов %s" % cur_numb.step)
                     flag = False
                     continue
                 elif gameList[0] != 1:
                     for i in range(gameList[0], 10):  # когда не 0 только первый
-                        new_numb.make_move(i, "+")
-                        new_numb.make_move(i, "-")
+                        cur_numb.make_move(i, "+")
+                        cur_numb.make_move(i, "-")
                 else:
                     for i in range(9):  # когда первый 1
-                        new_numb.make_move(2, "+")
-                        new_numb.make_move(2, "-")
+                        cur_numb.make_move(2, "+")
+                        cur_numb.make_move(2, "-")
 
             elif len(move) != 2 or not move[0].isdigit():
                 print("Неправильно ты ходишь: вводи n+ или n-, n=1..9")
                 continue
 
             else:
-                new_numb.make_move(*move)
+                cur_numb.make_move(*move)
 
             if list_int(gameList) == 0:
-                print("Поздравляю, ты выйграл! Шагов %s" % new_numb.step)
+                print("Поздравляю, ты выйграл! Шагов %s" % cur_numb.step)
                 #print(new_numb.gameList)
                 flag = False
             elif list_int(gameList) == 123456789:
-                print("Поздравляю, ты проиграл! Шагов %s" % new_numb.step)
+                print("Поздравляю, ты проиграл! Шагов %s" % cur_numb.step)
                 flag = False
     # else: print("Пока")
     elif hi == "n":
